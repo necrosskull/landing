946563055c26b98be5ade2b7e6bf5beed2cc77bd
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-08-26 07:46:20 +0300

    update: upload

---
+++ app/page.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <main className="flex flex-col scale-90 items-center justify-center min-h-screen lg:scale-100 relative">
+            <div className="absolute inset-0 z-0 filter blur-3xl opacity-60">
+                <Image
+                    src="/necrosskull.png"
+                    alt="Background Image"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+
+            <div className="z-10 hover:scale-105 transform transition duration-500 ease-in-out p-10 rounded-2xl">
+
+            <div className="flex flex-col select-none items-center justify-center">
+
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+                <h1 className="text-6xl font-bold text-center hover:text-purple-300 transform transition duration-200 ease-in-out">necrosskull</h1>
+            </div>
+
+            <div className="flex text-2xl justify-around items-center mt-10">
+                  <h1 className="font-bold text-center transform transition duration-300 hover:text-purple-300">
+                    <a href="https://github.com/necrosskull" target="_blank">github</a>
+                  </h1>
+                  <h1 className="font-bold text-center transform transition duration-300 hover:text-purple-300">
+                      <a href="https://github.com/necrosskull" target="_blank">telegram</a>
+                  </h1>
+            </div>
+      </div>
+    </main>
+  )
+}
